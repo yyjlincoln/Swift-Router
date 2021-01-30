6e710a275be40b77f6fe67fb669769b07988ce85
Lincoln Yan <yyjlincoln@gmail.com>
2021-01-30 22:05:56 +1100

    Initial Commit

---
+++ swift-router/swift_routerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_routerApp.swift
+//  swift-router
+//
+//  Created by Lincoln on 30/1/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_routerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
