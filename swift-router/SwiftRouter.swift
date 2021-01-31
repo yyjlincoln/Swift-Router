c7b308e6a25a73c3a0adac4d5f443c22b47192bd
Lincoln Yan <yyjlincoln@gmail.com>
2021-01-31 11:32:46 +1100

    Update: Now the destination view can accept "router : SwiftRouter" and "route : SwiftRouterRouteInstance" as an argument; Added "router.current" and "router.previous" as parameter; Added data support

--- swift-router/SwiftRouter.swift
+++ swift-router/SwiftRouter.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 
 struct SwiftRouterCriticalErrorView: View {
+    @EnvironmentObject var router: SwiftRouter
+    @EnvironmentObject var route: SwiftRouterRouteInstance
     @State var message: String
     var body: some View{
         Text("\(message)")
+//        Text("Current route: \(route.name)")
+//        Text("Previous route: \(router.Previous?.name ?? "<None>")")
     }
 }
 
@@ -24,18 +28,51 @@ struct SwiftRouterRoute {
     var name : String
     var view : AnyView
     var handlers : RouteHandlers = RouteHandlers()
+    var meta: Dictionary<String, Any>? = [:]
+}
+
+class SwiftRouterRouteInstance: ObservableObject {
+    public var name: String
+    public var view : AnyView
+    public var handlers : RouteHandlers = RouteHandlers()
+    public var meta: Dictionary<String, Any>? = [:]
+    public var data: Dictionary<String, Any>? = [:]
+    init(name: String, view: AnyView, handlers: RouteHandlers, meta: Dictionary<String, Any>?, data: Dictionary<String, Any>?) {
+        self.name=name
+        self.view=view
+        self.handlers=handlers
+        self.meta=meta
+        self.data=data
+    }
 }
 
 typealias RouterResponder = () -> AnyView?
-typealias RouterJudge = (SwiftRouterRoute) -> Void
+typealias RouterJudge = (SwiftRouterRouteInstance) -> Void
 
 
-class SwiftRouter{
+class SwiftRouter: ObservableObject{
+    // Now the handlers
     @Binding var RouterView: RouterResponder?
     private var routes: [SwiftRouterRoute]
     private var defaultroute: String?
     private var errorroute: String?
-    private var current: SwiftRouterRoute?
+    private var current: SwiftRouterRouteInstance?
+    private var previous: SwiftRouterRouteInstance?
+    public var Current: SwiftRouterRouteInstance? {
+        get{
+            return current
+        }
+    }
+    public var Routes: [SwiftRouterRoute]? {
+        get{
+            return routes
+        }
+    }
+    public var Previous: SwiftRouterRouteInstance? {
+        get{
+            return previous
+        }
+    }
     
     public init(routes: [SwiftRouterRoute] = [SwiftRouterRoute(name: "error", view: AnyView(SwiftRouterCriticalErrorView(message: "SwiftRouter Default Error Page: An error occured. Did you define 'routes' when init? Does the destination route exist?")))], RouterView : Binding<RouterResponder?>, defaultroute: String? = nil, errorroute: String? = "error"){
         self.routes = routes
@@ -62,7 +99,7 @@ class SwiftRouter{
         // Error - Could not even match the error route
         return nil
     }
-    func PushRoute(_ name: String) {
+    func PushRoute(_ name: String, _ data: Dictionary<String, Any>? = [:]) {
         // When PushRoute is called, it changes the binded RouteView(),
         // which will result in an UI Update and call RouterView().
         // RouterView() will then match the route, and return the corresponding AnyView()
@@ -70,6 +107,8 @@ class SwiftRouter{
         
         RouterView = {()->AnyView? in
             if let destinationRoute = self.MatchRoute(name) {
+                let destinationInstance = SwiftRouterRouteInstance(name: destinationRoute.name, view: destinationRoute.view, handlers: destinationRoute.handlers, meta: destinationRoute.meta, data: data)
+                
                 // Route matched, check handlers
                 if self.current != nil {
                     // Invoke willDestroy for previous route
@@ -79,11 +118,13 @@ class SwiftRouter{
                 }
                 // Invoke willCreate for previous route
                 if destinationRoute.handlers.willCreate != nil {
-                    destinationRoute.handlers.willCreate!(destinationRoute)
+                    destinationRoute.handlers.willCreate!(destinationInstance)
                 }
                 //
-                self.current = destinationRoute
-                return destinationRoute.view
+                self.previous = self.current
+                self.current = destinationInstance
+                
+                return AnyView(destinationRoute.view.environmentObject(self).environmentObject(destinationInstance))
             }
             return self.current?.view
         }
